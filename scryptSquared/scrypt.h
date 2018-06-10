e1edacdf984f87e8744f99a831fe34609132dfda
Paul Georgiou <pavlos1998@gmail.com>
2018-06-10 11:05:13 +0300

    Fix hash function

--- scryptSquared/scrypt.h
+++ scryptSquared/scrypt.h
@@ -7,7 +7,6 @@ extern "C" {
 
 void scrypt_1048576_1_1_256(const char* input, char* output);
 void scrypt_1048576_1_1_256_sp(const char* input, char* output, char* scratchpad);
-const int scrypt_scratchpad_size = 134218239;
 
 #ifdef __cplusplus
 }
