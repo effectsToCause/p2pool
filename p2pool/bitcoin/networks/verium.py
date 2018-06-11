6b9121c370d621e07a3411c3155569a72649dac7
Paul Georgiou <pavlos1998@gmail.com>
2018-06-11 19:06:41 +0300

    Verium params adjustment

--- p2pool/bitcoin/networks/verium.py
+++ p2pool/bitcoin/networks/verium.py
@@ -6,15 +6,16 @@ from twisted.internet import defer
 from .. import data, helper
 from p2pool.util import pack
 
+import scryptSquared as scrypt2
 
 P2P_PREFIX = '70352205'.decode('hex')
 P2P_PORT = 36988
 ADDRESS_VERSION = 70
 RPC_PORT = 33987
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue('veriumaddress' in (yield bitcoind.rpc_help()) and not (yield bitcoind.rpc_getinfo())['testnet']))
-SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
-POW_FUNC = data.hash256
-BLOCK_PERIOD = 600 # s
+SUBSIDY_FUNC = lambda height: 1000 # good enough for now
+POW_FUNC = lambda data: pack.IntType(256).unpack(scrypt2.getPoWHash(data))
+BLOCK_PERIOD = 300 # s
 SYMBOL = 'VRM'
 CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Verium') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Verium/') if platform.system() == 'Darwin' else os.path.expanduser('~/.verium'), 'verium.conf')
 BLOCK_EXPLORER_URL_PREFIX = 'https://chainz.cryptoid.info/vrm/block.dws?'
