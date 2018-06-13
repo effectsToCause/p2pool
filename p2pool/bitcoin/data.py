b297097f0c7e0eb81cfd48e41f5ac9da979e7d0e
Paul Georgiou <pavlos1998@gmail.com>
2018-06-14 02:43:25 +0300

    Fix difficulty

--- p2pool/bitcoin/data.py
+++ p2pool/bitcoin/data.py
@@ -239,12 +239,12 @@ def average_attempts_to_target(average_attempts):
 def target_to_difficulty(target):
     assert 0 <= target and isinstance(target, (int, long)), target
     if target >= 2**256: warnings.warn('target >= 2**256!')
-    return (0xffff0000 * 2**(256-64) + 1)/(target + 1)
+    return 2**(256-16) / (target + 1)
 
 def difficulty_to_target(difficulty):
     assert difficulty >= 0
     if difficulty == 0: return 2**256-1
-    return min(int((0xffff0000 * 2**(256-64) + 1)/difficulty - 1 + 0.5), 2**256-1)
+    return min(int(2**(256-16) / difficulty - 1 + 0.5), 2**256-1)
 
 # human addresses
 
